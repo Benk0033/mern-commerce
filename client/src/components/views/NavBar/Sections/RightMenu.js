6093550c24b561e89c9f1faa0e8aa034d52877ae
Benk0033 <benk0033@gmail.com>
2020-12-30 21:52:44 -0800

    Add totals and remove items from cart

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -37,8 +37,8 @@ function RightMenu(props) {
           <a href="/product/upload">Upload</a>
         </Menu.Item>
 
-        <Menu.Item key="cart">
-          <Badge count={0}>
+        <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
+          <Badge count={user.userData && user.userData.cart.length}>
             <a href="/user/cart" style={{ marginRight: -22, color: '#667777' }}>
               <Icon type="shopping-cart" style={{ fontSize: 30, marginBottom: 4 }} />
             </a>
