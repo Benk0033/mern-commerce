c8d5fd24943613e074f9000a603b434f1c495eea
Benk0033 <benk0033@gmail.com>
2021-01-02 22:05:57 -0800

    added PayPal Checkout

---
+++ client/src/components/views/LandingPage/Sections/Data.js
@@ -0,0 +1,78 @@
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $500",
+        "array": [0, 500]
+    },
+    {
+        "_id": 2,
+        "name": "$501 to $1000",
+        "array": [501, 1000]
+    },
+    {
+        "_id": 3,
+        "name": "$1001 to $1500",
+        "array": [1001, 1500]
+    },
+    {
+        "_id": 4,
+        "name": "$1501 to $2000",
+        "array": [1501, 2000]
+    },
+    {
+        "_id": 5,
+        "name": "$2001 to $2500",
+        "array": [2001, 2500]
+    },
+    {
+        "_id": 6,
+        "name": "$2501 to $3000",
+        "array": [2501, 3000]
+    },
+    {
+        "_id": 7,
+        "name": "More than $3000",
+        "array": [3001, 1000000]
+    }
+]
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Africa"
+    },
+    {
+        "_id": 2,
+        "name": "Europe"
+    },
+    {
+        "_id": 3,
+        "name": "Asia"
+    },
+    {
+        "_id": 4,
+        "name": "North America"
+    },
+    {
+        "_id": 5,
+        "name": "South America"
+    },
+    {
+        "_id": 6,
+        "name": "Australia"
+    },
+    {
+        "_id": 7,
+        "name": "Antartica"
+    }
+]
+
+export {
+    price,
+    continents
+}
