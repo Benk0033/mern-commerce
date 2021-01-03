d8c90d818d7ee93e6d34df6d35e271e07a7fa47b
Benk0033 <benk0033@gmail.com>
2021-01-02 22:04:07 -0800

    Added PayPal Checkout

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { User } = require("../models/User");
 const { Product } = require("../models/Product");
 const { auth } = require("../middleware/auth");
+const { Payment } = require('../models/Payment');
+const async = require('async');
 
 //=================================
 //             User
@@ -158,6 +160,89 @@ router.get('/userCartInfo', auth, (req, res) => {
                     return res.status(200).json({ success: true, cartDetail, cart })
                 })
 
+        }
+    )
+})
+
+router.post('/successBuy', auth, (req, res) => {
+    let history = [];
+    let transactionData = {};
+
+    // 1. Put payment info inside the User Collection
+    req.body.cartDetail.forEach((item) => {
+        history.push({
+            dateOfPurchase: Date.now(),
+            name: item.title,
+            id: item._id,
+            price: item.price,
+            quantity: item.quantity,
+            paymentId: req.body.paymentData.paymentID
+        })
+    })
+
+    // 2. Put payment information from PayPal into payment collection
+
+    transactionData.user = {
+        id: req.user._id,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        email: req.user.email
+    }
+
+    transactionData.data = req.body.paymentData;
+    transactionData.product = history
+
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $push: { history: history }, $set: { cart: [] } },
+        { new: true },
+        (err, user) => {
+
+            if (err) return res.json({ success: false, err });
+
+            const payment = new Payment(transactionData)
+            payment.save((err, doc) => {
+                if (err) return res.json({ success: false, err });
+
+                // 3. increase the amount of number for the sold information
+
+                let products = [];
+                doc.product.forEach(item => {
+                    products.push({ id: item.id, quantity: item.quantity })
+                })
+
+                async.eachSeries(products, (item, callback) => {
+                    Product.update(
+                        { _id: item.id },
+                        {
+                            $inc: {
+                                "sold": item.quantity
+                            }
+                        },
+                        { new: false },
+                        callback
+                    )
+                }, (err) => {
+                    if (err) return res.json({ success: false, err })
+                    res.status(200).json({
+                        success: true,
+                        cart: user.cart,
+                        cartDetail: []
+                    })
+                })
+            })
+        }
+    )
+
+})
+
+router.get('/getHistory', auth, (req, res) => {
+    User.findOne(
+        { _id: req.user._id },
+        (err, doc) => {
+            let history = doc.history;
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, history })
         }
     )
 })
